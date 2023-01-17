2c31e9547a16126af8cd78d644e58ae464ba2ff2
Windostre <windostre@gmail.com>
2023-01-17 18:04:39 +0300

    добавлены ручки

--- src/test/java/site/nomoreparties/stellarburgers/helpers/Constants.java
+++ src/test/java/site/nomoreparties/stellarburgers/helpers/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     protected final String API_USER_DATA = "/api/auth/user"; // для получения, обновления, удаления данных пользователя, и
     protected final String API_USER_LOGIN = "/api/auth/login"; // авторизация
     protected final String API_USER_LOGOUT = "/api/auth/logout"; // выход из системы
+    protected final String API_INGREDIENTS = "/api/ingredients"; // список ингридиентов
+    protected final String API_ORDERS = "/api/orders"; // для создания заказлв
     protected final String CONSTANT_USER_EMAIL = "test@testmail.com";
     protected final String CONSTANT_USER_PASSWORD = "test_password";
     protected final String CONSTANT_USER_NAME = "TestName";
